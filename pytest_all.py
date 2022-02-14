7b164db5fcc2dcdb639736dbff42628ac11fdeea
J A <jand@blah.blah>
2022-02-13 21:04:59 -0500

    update of data

--- pytest_all.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from pathlib import Path
-from subprocess import run
-
-if __name__ == "__main__":
-    app_folders = [f for f in Path().cwd().glob('*') if f.is_dir()
-    if f.name.startswith('app')]
-
-    [run(['pytest', f]) for f in app_folders]
